a9040eafdf79e6e63248d58c537501b3e800264b
Morgan Powers <morgan.c.powers@gmail.com>
2026-03-30 11:09:46 -0500

    added switch values

--- analysis.r
+++ analysis.r
@@ -4,6 +4,9 @@ source("data.r")
 printf <- function(desc,dat){
     print(noquote(paste0(desc,": ",dat)))
 }
+#use partisan lean variable vs voted
+#D -> R, R -> D, and global model
+printf("mean Harris thermometer rating",mean(anes$harrisTherm,na.rm=T))
+printf("mean Trump thermometer rating",mean(anes$trumpTherm,na.rm=T))
 
-printf("mean Harris thermometer rating",mean(harrisTherm,na.rm=T))
-printf("mean Trump thermometer rating",mean(trumpTherm,na.rm=T))
+print(table(leftTheLeft))
--- data.r
+++ data.r
@@ -2,9 +2,23 @@ options(repos = "https://cran.rstudio.com")
 
 anes <- read.csv('./anes2024.csv')
 
-harrisTherm <- ifelse(anes$V241156<0 | anes$V241156>100,NA,anes$V241156)
-trumpTherm <- ifelse(anes$V241157<0 | anes$V241157>100,NA,anes$V241157)
-voted <- ifelse(anes$V242067<0,NA,anes$V242067)
+#If an element is a feature, change to one, otherwise change it to 0
+#This allows correlation checking between non-numeric values
+numericize <- function(lst,cond){ as.numeric(lst == cond) }
+
+anes$harrisTherm <- ifelse(anes$V241156<0 | anes$V241156>100,NA,anes$V241156)
+anes$trumpTherm <- ifelse(anes$V241157<0 | anes$V241157>100,NA,anes$V241157)
+#1: Harris; 2: Trump; other
+anes$voted <- ifelse(anes$V242067>2,"Other",ifelse(anes$V242067<0,NA,anes$V242067))
+#7-pt scale; lower is liberal
+anes$bias <- ifelse(anes$V241177<0 | anes$V241177>7,NA,anes$V241177)
+
+#politically liberal, voted for Trump
+leftTheLeft <- subset(anes,bias<4 & voted == 2)
+#politically conservative, voted for Harris
+renouncedTheRight <- subset(anes,bias>4 & voted == 1)
+#all people who voted against their political leaning
+switched <- rbind(anes$leftTheLeft,anes$renouncedTheRight)
 
 #1 if they prefer Harris to Trump else 0
 anes$pref <- ifelse(anes$V241156>anes$V241157,1,0)
