929eac58e0be1d3a0312b7724766180d49b8aaeb
Morgan Powers <morgan.c.powers@gmail.com>
2026-03-30 18:52:35 -0500

    added financial worry

--- analysis.r
+++ analysis.r
@@ -4,9 +4,20 @@ source("data.r")
 printf <- function(desc,dat){
     print(noquote(paste0(desc,": ",dat)))
 }
-#use partisan lean variable vs voted
-#D -> R, R -> D, and global model
+
 printf("mean Harris thermometer rating",mean(anes$harrisTherm,na.rm=T))
 printf("mean Trump thermometer rating",mean(anes$trumpTherm,na.rm=T))
-
-print(table(leftTheLeft))
+cat("
+  ####################
+ ## Economic worry ##
+####################
+")
+cat(noquote("Voters who consider themselves liberal but voted for Trump\nfrom most to least worried about their financial situation."))
+print(table(leftTheLeft$worry))
+cat(noquote("Voters who consider themselves conservative but voted for Harris\nfrom most to least worried about their financial situation."))
+print(table(renouncedTheRight$worry))
+cat(noquote("Voters who voted against their ideological bias\nfrom most to least worried about their financial situation."))
+print(table(switched$worry))
+worry <- c(1,2,3,4,5)
+worriers <- c(7,10,27,55,75)
+summary(lm(worriers ~ worry))
--- data.r
+++ data.r
@@ -8,17 +8,25 @@ numericize <- function(lst,cond){ as.numeric(lst == cond) }
 
 anes$harrisTherm <- ifelse(anes$V241156<0 | anes$V241156>100,NA,anes$V241156)
 anes$trumpTherm <- ifelse(anes$V241157<0 | anes$V241157>100,NA,anes$V241157)
+
+  ############
+ ## Issues ##
+############
 #1: Harris; 2: Trump; other
 anes$voted <- ifelse(anes$V242067>2,"Other",ifelse(anes$V242067<0,NA,anes$V242067))
 #7-pt scale; lower is liberal
 anes$bias <- ifelse(anes$V241177<0 | anes$V241177>7,NA,anes$V241177)
+#financial worry; lower is more worried
+anes$worry <- ifelse(anes$V241539 < 0,NA,anes$V241539)
 
+  ################
+ ## Assemblage ##
+################
+#1 if they prefer Harris to Trump else 0
+anes$pref <- ifelse(anes$V241156>anes$V241157,1,0)
 #politically liberal, voted for Trump
 leftTheLeft <- subset(anes,bias<4 & voted == 2)
 #politically conservative, voted for Harris
 renouncedTheRight <- subset(anes,bias>4 & voted == 1)
 #all people who voted against their political leaning
-switched <- rbind(anes$leftTheLeft,anes$renouncedTheRight)
-
-#1 if they prefer Harris to Trump else 0
-anes$pref <- ifelse(anes$V241156>anes$V241157,1,0)
+switched <- rbind(leftTheLeft,renouncedTheRight)
