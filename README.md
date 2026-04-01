2e75fc32b88ac4350b3a6c6ba1422374515229a2
Morgan Powers <morgan.c.powers@gmail.com>
2026-03-31 22:37:59 -0500

    added crime

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ResearchMethods-FinalProject
 The final project for Research Methods in Political Science (PLSI-3372).
+
+Parsing and formatting data is handled in data.r.  
+Analysis and documentation occurs in analysis.r.
--- data.r
+++ data.r
@@ -1,23 +1,32 @@
 options(repos = "https://cran.rstudio.com")
-
 anes <- read.csv('./anes2024.csv')
 
+  ###############
+ ## Functions ##
+###############
+#print data with description
+printf <- function(desc,dat){ prints(paste0(desc,": ",dat)) }
+#print string without quotes
+prints <- function(str){ cat(noquote(str),fill=T) }
 #If an element is a feature, change to one, otherwise change it to 0
 #This allows correlation checking between non-numeric values
+#as.numeric interprets 1 and 0 as their boolean counterparts
 numericize <- function(lst,cond){ as.numeric(lst == cond) }
-
-anes$harrisTherm <- ifelse(anes$V241156<0 | anes$V241156>100,NA,anes$V241156)
-anes$trumpTherm <- ifelse(anes$V241157<0 | anes$V241157>100,NA,anes$V241157)
 
   ############
  ## Issues ##
 ############
-#1: Harris; 2: Trump; other
+#Trump and Harris feeling thermometers
+anes$harrisTherm <- ifelse(anes$V241156<0 | anes$V241156>100,NA,anes$V241156)
+anes$trumpTherm <- ifelse(anes$V241157<0 | anes$V241157>100,NA,anes$V241157)
+#who n voted for - 1: Harris; 2: Trump; other
 anes$voted <- ifelse(anes$V242067>2,"Other",ifelse(anes$V242067<0,NA,anes$V242067))
 #7-pt scale; lower is liberal
 anes$bias <- ifelse(anes$V241177<0 | anes$V241177>7,NA,anes$V241177)
 #financial worry; lower is more worried
 anes$worry <- ifelse(anes$V241539 < 0,NA,anes$V241539)
+#5-point scale; lower means crime is more important
+anes$crime <- ifelse(anes$V242177 < 0,NA,anes$V242177)
 
   ################
  ## Assemblage ##
@@ -25,8 +34,8 @@ anes$worry <- ifelse(anes$V241539 < 0,NA,anes$V241539)
 #1 if they prefer Harris to Trump else 0
 anes$pref <- ifelse(anes$V241156>anes$V241157,1,0)
 #politically liberal, voted for Trump
-leftTheLeft <- subset(anes,bias<4 & voted == 2)
+anes$leftTheLeft <- as.numeric(anes$bias<4 & anes$voted == 2)
 #politically conservative, voted for Harris
-renouncedTheRight <- subset(anes,bias>4 & voted == 1)
+anes$renouncedTheRight <- as.numeric(anes$bias>4 & anes$voted == 1)
 #all people who voted against their political leaning
-switched <- rbind(leftTheLeft,renouncedTheRight)
+anes$switch <- as.numeric(anes$leftTheLeft | anes$renouncedTheRight)
